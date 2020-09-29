7e34bc955d14536ae1a3ee444fa67f2a16a86971
Valmir Barbosa <valmir.php@gmail.com>
2020-09-29 00:00:37 -0300

    chore: first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
+
+
+@Controller()
+export class AppController {
+
+  @Get()
+  getHello(@Req() req: Request): string {
+    return 'Hello World';
+  }
+
+}
