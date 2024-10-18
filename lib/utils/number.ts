0cc176d63a12f55f0a0936003f7aef19e6b970f7
Valmir Barbosa <valmir.php@gmail.com>
2024-10-18 13:17:31 -0300

    fix(utils): ajuste func currencyFormat

--- lib/utils/number.ts
+++ lib/utils/number.ts
@@ -1,6 +1,6 @@
 export function currencyFormat(number, digits = 2) {
-  return (parseFloat(number) || 0)
-    .toFixed(digits)
-    .replace('.', ',')
-    .replace(/(\d)(?=(\d{4})+(?!\d))/g, '$1.');
+  return new Intl.NumberFormat('pt-BR', {
+    minimumFractionDigits: digits,
+    maximumFractionDigits: digits,
+  }).format(number);
 }
